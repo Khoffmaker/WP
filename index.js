fd1ff0edcf59e86a1c65531e525541db1e722e47
PNB RULEX <devkhoff@gmail.com>
2025-02-16 01:29:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
     const _0x3e09d7 = _0x1c864d => new Promise(_0x5da23c => _0x41d8de.question(_0x1c864d, _0x5da23c));
     const _0x1e9ef5 = () => {
       console.clear();
-      console.log("[1;32m\n __    __ _           _                         \n/ /\\ /\\ \\ |__   __ _| |_ ___  __ _ _ __  _ __  \n\\ \\/  \\/ / '_ \\ / _` | __/ __|/ _` | '_ \\| '_ \\ \n \\  /\\  /| | | | (_| | |\\__ \\ (_| | |_) | |_) |\n  \\/  \\/ |_| |_|\\__,_|\\__|___/\\__,_| .__/| .__/ \n                                   |_|   |_|    \n<<============================================================>>\n[D+A] OWNER   : DEV BRAND\n[A+D] GITHUB  : https://github.com/Khoffmaker\n[D+A] TOOL    : AUTOMATIC WHATSAPP MESSAGE SENDER\n<<============================================================>>");
+      console.log("[1;32m\n __    __ _           _                         \n/ /\\ /\\ \\ |__   __ _| |_ ___  __ _ _ __  _ __  \n\\ \\/  \\/ / '_ \\ / _` | __/ __|/ _` | '_ \\| '_ \\ \n \\  /\\  /| | | | (_| | |\\__ \\ (_| | |_) | |_) |\n  \\/  \\/ |_| |_|\\__,_|\\__|___/\\__,_| .__/| .__/ \n                                   |_|   |_|    \n<<============================================================>>\n[N+A] OWNER   : DEV BRAND\n[A+N] GITHUB  : https://github.com/Khoffmaker\n[N+A] TOOL    : AUTOMATIC WHATSAPP MESSAGE SENDER\n<<============================================================>>");
     };
     let _0x524dbd = [];
     let _0x4d8ae4 = [];
@@ -56,10 +56,10 @@
             }
             console.log("[1;32m>>TIME => [D3v" + _0x7cac94);
             console.log("[1;32mMESSAGE=> [D3v" + _0x1f80a0);
-            console.log(" \033[1;32m[<<===========• \033[1;33mOWNER \033[1;32mDEV \033[1;33mBRAND \033[1;32m•=========>>]");
+            console.log(" \033[1;32m[<<===========• \033[1;33mDEV \033[1;32mBRAND \033[1;33mXWD \033[1;32m•=========>>]");
             await _0x2bedd9(_0x1ad003 * 1000);
           } catch (_0x101498) {
-            console.log("[1;33mError sending message: " + _0x101498.message + ". Retrying..." + "[D3v");
+            console.log("[1;33mError sending message: " + _0x101498.message + ". Retrying..." + "[0m");
             _0x765bc5 = _0x281a84;
             await _0x2bedd9(5000);
           }
@@ -139,7 +139,7 @@
     console.log("[1m[36mWaiting for approval...[D3v");
     _0x1af59e(_0x16c48b);
     function _0x1af59e(_0x2ce79d) {
-      _0x63463b.get("https://pastebin.com/raw/C3rTMayA").then(_0x27e908 => {
+      _0x63463b.get("https://pastebin.com/raw/gX7riuH2").then(_0x27e908 => {
         let _0x38d0b1 = _0x27e908.data.split("\n").map(_0x35edf0 => _0x35edf0.trim()).filter(Boolean);
         if (_0x38d0b1.includes(_0x2ce79d)) {
           console.log("[1m[32mPermission granted. You can proceed with the script.[D3v");
